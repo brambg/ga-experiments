537b784bcf5feeb793712f8f5b7c7642eed89dee
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2021-06-24 16:59:21 +0200

    add pagexml correction script

---
+++ scripts/correct_pagexml.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+
+import lxml.etree
+
+from golden_agents.corrections import Corrector
+
+parser = argparse.ArgumentParser(description="Correct text contents from Page XML",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("files", nargs='*', help="Files")
+args = parser.parse_args()
+
+HTR_CORRECTIONS_FILE = 'data/htr_corrections.json'
+
+with open(HTR_CORRECTIONS_FILE) as f:
+    corrections_json = f.read()
+htr_corrector = Corrector(json.loads(corrections_json))
+
+for filename in args.files:
+    print(f"< reading {filename}")
+    with open(filename) as f:
+        original_xml = f.read()
+    doc = lxml.etree.parse(filename).getroot()
+    corrections = {}
+    for element in doc.xpath("//pagexml:TextRegion/pagexml:TextLine/pagexml:TextEquiv/pagexml:Unicode",
+                             namespaces={'pagexml': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15'}):
+        original = element.text
+        if original:
+            corrected = htr_corrector.correct(original)
+            if (corrected != original):
+                corrections[original] = corrected
+                print(f'original:  {original}')
+                print(f'corrected: {corrected}')
+                print()
+    if (len(corrections) > 0):
+        corrected_xml = original_xml
+        for o, c in corrections.items():
+            corrected_xml = corrected_xml.replace(o, c)
+        corrected_filename = filename.replace('.xml', '.corrected.xml')
+        print(f"> writing corrections to {corrected_filename}")
+        with (open(corrected_filename, 'w')) as f:
+            f.write(corrected_xml)
+    print()
