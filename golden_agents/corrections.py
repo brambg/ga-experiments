6309b6a9cee5a26de11436772d259c8d0fb67730
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2021-06-24 15:49:55 +0200

    fix correction for ngram terms

--- golden_agents/corrections.py
+++ golden_agents/corrections.py
@@ -1,4 +1,6 @@
 # from analiticcl import VariantModel, Weights, SearchParameters
+from nltk import ngrams
+
 
 class Corrector():
 
@@ -9,4 +11,23 @@ class Corrector():
     def correct(self, raw_str: str) -> str:
         words = raw_str.split(' ')
         corrected = ' '.join([self.corrections.get(o, o) for o in words])
+        for n in range(2, self.max_ngram + 1):
+            words = corrected.split(' ')
+            ngram_corrections = {}
+            for i, t in enumerate(ngrams(words, n)):
+                orig = " ".join(t)
+                if (orig in self.corrections):
+                    ngram_corrections[i] = self.corrections[orig]
+            if (len(ngram_corrections) > 0):
+                corrected_terms = []
+                i = 0
+                while (i < len(words)):
+                    if (i in ngram_corrections):
+                        corrected_terms.append(ngram_corrections[i])
+                        i += n
+                    else:
+                        corrected_terms.append(words[i])
+                        i += 1
+                corrected = ' '.join(corrected_terms)
+
         return corrected
--- tests/test_corrector.py
+++ tests/test_corrector.py
@@ -22,6 +22,20 @@ class CorrectorTestCase(unittest.TestCase):
         c = Corrector(corrections)
         self.assertEqual(4, c.max_ngram)
 
+    def test_corrector_with_bigrams(self):
+        corrections = {"term1": "term2", 'cor rect': 'correct'}
+        c = Corrector(corrections)
+        orig = "I say this is cor rect for now"
+        expected = "I say this is correct for now"
+        self.assertEqual(expected, c.correct(orig))
+
+    def test_corrector_with_trigrams(self):
+        corrections = {'als me de': 'alsmede'}
+        c = Corrector(corrections)
+        orig = "proloog als me de epiloog"
+        expected = "proloog alsmede epiloog"
+        self.assertEqual(expected, c.correct(orig))
+
     def test_htr_corrections_1(self):
         orig = "d'langstlevenden Abrahan aelites aftestaen"
         expected = "d' langstlevenden Abraham ad lites af te staen"
@@ -33,8 +47,8 @@ class CorrectorTestCase(unittest.TestCase):
         self.assert_htr_corrections(orig, expected)
 
     def test_htr_corrections_with_bigram(self):
-        orig = 'de biteur en de biteuren'
-        expected = 'debiteur en debiteuren'
+        orig = 'Alsook de biteur en de biteuren heden'
+        expected = 'Alsook debiteur en debiteuren heden'
         self.assert_htr_corrections(orig, expected)
 
     def assert_htr_corrections(self, orig, expected):
