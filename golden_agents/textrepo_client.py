c56fc8b141e981980e3ca1e173543127e71c0f06
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2021-07-07 15:49:30 +0200

    add more endpoint processing to textrepo client

--- golden_agents/textrepo_client.py
+++ golden_agents/textrepo_client.py
@@ -1,11 +1,13 @@
 import uuid
+from dataclasses import dataclass, field
 from datetime import datetime
 from http import HTTPStatus
+from typing import List
 
 import requests
-from dataclasses import dataclass
+from dataclasses_json import dataclass_json, config
 from dateutil.parser import isoparse
-from icecream import ic
+from marshmallow import fields
 from requests import Response
 
 
@@ -24,6 +26,36 @@ class DocumentsPage:
     total: int
 
 
+@dataclass
+class FileIdentifier:
+    id: uuid
+    docId: uuid
+    typeId: int
+    url: str
+
+
+@dataclass_json
+@dataclass
+class VersionIdentifier:
+    id: uuid
+    fileId: uuid
+    createdAt: datetime = field(
+        metadata=config(
+            encoder=datetime.isoformat,
+            decoder=datetime.fromisoformat,
+            mm_field=fields.DateTime(format='iso')
+        ))
+    contentsSha: str
+
+
+@dataclass_json
+@dataclass
+class FileType:
+    id: int
+    name: str
+    mimetype: str
+
+
 class TextRepoClient():
 
     def __init__(self, base_uri: str):
@@ -36,7 +68,7 @@ class TextRepoClient():
     def __repr__(self):
         return self.__str__()
 
-    def get_about(self):
+    def get_about(self) -> dict:
         url = f'{self.base_uri}/'
         response = requests.get(url=url)
         return self.__handle_response(response, {HTTPStatus.OK: lambda r: r.json()})
@@ -45,7 +77,8 @@ class TextRepoClient():
                        externalId: str = None,
                        createdAfter: datetime = None,
                        limit: int = None,
-                       offset: int = None):
+                       offset: int = None
+                       ) -> DocumentsPage:
         url = f'{self.base_uri}/rest/documents'
         params = {}
         if (externalId):
@@ -59,20 +92,153 @@ class TextRepoClient():
         response = requests.get(url=url, params=params)
         return self.__handle_response(response, {HTTPStatus.OK: to_documents_page})
 
-    def create_document(self, externalId: str):
+    def create_document(self, externalId: str) -> DocumentIdentifier:
         url = f'{self.base_uri}/rest/documents'
         response = requests.post(url=url, json={"externalId": externalId})
+        return self.__handle_response(response, {HTTPStatus.CREATED: to_document_identifier})
+
+    def update_document_externalId(self, documentId: DocumentIdentifier, externalId: str) -> DocumentIdentifier:
+        url = f'{self.base_uri}/rest/documents/{documentId.id}'
+        response = requests.put(url=url, json={"externalId": externalId})
         return self.__handle_response(response, {HTTPStatus.OK: to_document_identifier})
 
-    def read_document(self, documentIdentifier: DocumentIdentifier):
+    def read_document(self, documentIdentifier: DocumentIdentifier) -> DocumentIdentifier:
+        """Retrieve document"""
         url = f'{self.base_uri}/rest/documents/{documentIdentifier.id}'
         response = requests.get(url=url)
         return self.__handle_response(response, {HTTPStatus.OK: to_document_identifier})
 
-    def delete_document(self, documentId: DocumentIdentifier):
+    def delete_document(self, documentId: DocumentIdentifier) -> bool:
+        """Delete document"""
         url = f'{self.base_uri}/rest/documents/{documentId.id}'
         response = requests.delete(url=url)
-        ic(response)
+        return self.__handle_response(response, {HTTPStatus.OK: lambda r: True})
+
+    def purge_document(self, externalId: str) -> bool:
+        """
+        Delete a document including its metadata, files, versions and contents. Contents are only deleted when not referenced by any other versions.
+
+        :param externalId: the external Id
+        :type externalId: str
+        :return: whether the purge succeeded
+        :rtype: bool
+        """
+        url = f'{self.base_uri}/task/delete/documents/{externalId}'
+        response = requests.delete(url=url)
+        return self.__handle_response(response, {HTTPStatus.OK: lambda r: True})
+
+    def set_document_metadata(self, documentIdentifier: DocumentIdentifier, key: str, value: str) -> dict:
+        url = f'{self.base_uri}/rest/documents/{documentIdentifier.id}/metadata/{key}'
+        response = requests.put(url=url, data=value)
+        return self.__handle_response(response, {HTTPStatus.OK: lambda r: r.json()})
+
+    def read_document_metadata(self, documentIdentifier: DocumentIdentifier) -> dict:
+        url = f'{self.base_uri}/rest/documents/{documentIdentifier.id}/metadata'
+        response = requests.get(url=url)
+        return self.__handle_response(response, {HTTPStatus.OK: lambda r: r.json()})
+
+    def delete_document_metadata(self, documentIdentifier: DocumentIdentifier, key: str) -> bool:
+        url = f'{self.base_uri}/rest/documents/{documentIdentifier.id}/metadata/{key}'
+        response = requests.delete(url=url)
+        return self.__handle_response(response, {HTTPStatus.OK: lambda r: True})
+
+    def create_document_file(self, documentIdentifier: DocumentIdentifier, typeId: int) -> FileIdentifier:
+        url = f'{self.base_uri}/rest/files'
+        response = requests.post(url=url, json={'docId': documentIdentifier.id, 'typeId': typeId})
+        return self.__handle_response(response, {HTTPStatus.CREATED: to_file_identifier})
+
+    def read_file(self, fileIdentifier: FileIdentifier) -> FileIdentifier:
+        url = f'{self.base_uri}/rest/files/{fileIdentifier.id}'
+        response = requests.get(url=url)
+        return self.__handle_response(response, {HTTPStatus.OK: to_file_identifier})
+
+    def update_document_file(self, documentIdentifier: DocumentIdentifier, typeId: int) -> FileIdentifier:
+        url = f'{self.base_uri}/rest/files'
+        response = requests.put(url=url, data={'docId': documentIdentifier.id, 'typeId': typeId})
+        return self.__handle_response(response, {HTTPStatus.OK: to_file_identifier})
+
+    def delete_file(self, fileIdentifier: FileIdentifier) -> bool:
+        url = f'{self.base_uri}/rest/files/{fileIdentifier.id}'
+        response = requests.delete(url=url)
+        return self.__handle_response(response, {HTTPStatus.OK: lambda r: True})
+
+    def read_document_files(self, documentIdentifier: DocumentIdentifier, limit: int = None,
+                            offset: int = None) -> dict:
+        url = f'{self.base_uri}/rest/documents/{documentIdentifier.id}/files'
+        params = {}
+        if (limit):
+            params['limit'] = limit
+        if (offset):
+            params['offset'] = offset
+        response = requests.get(url=url, params=params)
+        return self.__handle_response(response, {HTTPStatus.OK: lambda r: r.json()})
+
+    def read_file_metadata(self, fileIdentifier: FileIdentifier) -> dict:
+        url = f'{self.base_uri}/rest/files/{fileIdentifier.id}/metadata'
+        response = requests.get(url=url)
+        return self.__handle_response(response, {HTTPStatus.OK: lambda r: r.json()})
+
+    def set_file_metadata(self, fileIdentifier: FileIdentifier, key: str, value: str) -> bool:
+        """Create or update file metadata entry"""
+        url = f'{self.base_uri}/rest/files/{fileIdentifier.id}/metadata/{key}'
+        response = requests.put(url=url, data=value)
+        return self.__handle_response(response, {HTTPStatus.OK: lambda r: True})
+
+    def delete_file_metadata(self, fileIdentifier: FileIdentifier, key: str) -> bool:
+        """Delete file metadata entry"""
+        url = f'{self.base_uri}/rest/files/{fileIdentifier.id}/metadata/{key}'
+        response = requests.delete(url=url)
+        return self.__handle_response(response, {HTTPStatus.OK: lambda r: True})
+
+    def read_file_versions(self,
+                           fileIdentifier: FileIdentifier,
+                           limit: int = None,
+                           offset: int = None,
+                           createdAfter: datetime = None
+                           ) -> List[FileIdentifier]:
+        url = f'{self.base_uri}/rest/files/{fileIdentifier.id}/versions'
+        params = {}
+        if (createdAfter):
+            params['createdAfter'] = createdAfter.isoformat(timespec='seconds')
+        if (limit):
+            params['limit'] = limit
+        if (offset):
+            params['offset'] = offset
+        response = requests.get(url=url, params=params)
+        return self.__handle_response(response,
+                                      {HTTPStatus.OK: lambda r: [VersionIdentifier.from_dict(d) for d in
+                                                                 r.json()["items"]]})
+
+    def create_version(self, fileIdentifier: FileIdentifier, file) -> VersionIdentifier:
+        url = f'{self.base_uri}/rest/versions'
+        files = {'contents': file}
+        data = {'fileId': fileIdentifier.id}
+        response = requests.post(url=url, files=files, data=data)
+        return self.__handle_response(response, {HTTPStatus.CREATED: to_version_identifier})
+
+    def read_file_types(self) -> List[FileType]:
+        url = f'{self.base_uri}/rest/types'
+        response = requests.get(url=url)
+        return self.__handle_response(response, {HTTPStatus.OK: lambda r: [FileType.from_dict(d) for d in r.json()]})
+
+    def create_file_type(self, name: str, mimetype: str) -> FileType:
+        url = f'{self.base_uri}/rest/types'
+        response = requests.post(url=url, json={"name": name, "mimetype": mimetype})
+        return self.__handle_response(response, {HTTPStatus.CREATED: to_file_type})
+
+    def read_file_type(self, id: int) -> FileType:
+        url = f'{self.base_uri}/rest/types/{id}'
+        response = requests.get(url=url)
+        return self.__handle_response(response, {HTTPStatus.OK: to_file_type})
+
+    def update_file_type(self, id: int, name: str, mimetype: str) -> FileType:
+        url = f'{self.base_uri}/rest/types/{id}'
+        response = requests.put(url=url, json={"name": name, "mimetype": mimetype})
+        return self.__handle_response(response, {HTTPStatus.OK: to_file_type})
+
+    def delete_file_type(self, id: int):
+        url = f'{self.base_uri}/rest/types/{id}'
+        response = requests.delete(url=url)
         return self.__handle_response(response, {HTTPStatus.OK: lambda r: True})
 
     def __handle_response(self, response: Response, result_producers: dict):
@@ -98,6 +264,18 @@ def to_document_identifier(response: Response) -> DocumentIdentifier:
                               createdAt=isoparse(json['createdAt']))
 
 
+def to_file_identifier(response: Response) -> FileIdentifier:
+    json = response.json()
+    return FileIdentifier(id=json['id'],
+                          docId=json['docId'],
+                          typeId=json['typeId'],
+                          url=response.headers['location'])
+
+
+def to_version_identifier(response: Response) -> VersionIdentifier:
+    return VersionIdentifier.from_dict(response.json())
+
+
 def to_documents_page(response: Response) -> DocumentsPage:
     json = response.json()
     items = [DocumentIdentifier(id=j['id'],
@@ -108,3 +286,7 @@ def to_documents_page(response: Response) -> DocumentsPage:
                          page_limit=json['page']['limit'],
                          page_offset=json['page']['offset'],
                          total=json['total'])
+
+
+def to_file_type(response: Response) -> FileType:
+    return FileType.from_dict(response.json())
--- tests/test_textrepo.py
+++ tests/test_textrepo.py
@@ -1,5 +1,7 @@
 import unittest
+import uuid
 from datetime import datetime
+from io import StringIO
 
 from icecream import ic
 
@@ -47,16 +49,117 @@ class TextRepoTestCase(unittest.TestCase):
         self.assertIsNotNone(documentsPage)
         self.assertEqual(0, documentsPage.total)
 
-    def test_create_document(self):
-        externalId = 'ga:annotationId:pageId2'
+    def test_file_type_crud(self):
+        self.purge_file_types()
+
+        type = TR.create_file_type("httml", "application/text+html")
+        ic(type)
+        self.assertEqual("httml", type.name)
+        self.assertEqual("application/text+html", type.mimetype)
+
+        updated_type = TR.update_file_type(type.id, "html", "text/html")
+        self.assertEqual("html", updated_type.name)
+        self.assertEqual("text/html", updated_type.mimetype)
+        self.assertEqual(type.id, updated_type.id)
+
+        types = TR.read_file_types()
+        self.assertEqual(1, len(types))
+        self.assertEqual(updated_type, types[0])
+
+        ok = TR.delete_file_type(type.id)
+        assert ok
+
+        types = TR.read_file_types()
+        self.assertEqual(0, len(types))
+
+    def test_document_crud(self):
+        self.purge_all_documents()
+        self.purge_file_types()
+
+        externalId = f'ga:annotationId:{uuid.uuid4()}'
         documentId = TR.create_document(externalId)
         ic(documentId)
         self.assertIsNotNone(documentId)
 
         readId = TR.read_document(documentId)
         ic(readId)
-        success = TR.delete_document(readId)
-        ic(success)
+        self.assertEqual(documentId, readId)
+
+        files = TR.read_document_files(documentId)
+        ic(files)
+
+        result = TR.set_document_metadata(documentId, "field", "value")
+        ic(result)
+
+        metadata = TR.read_document_metadata(documentId)
+        ic(metadata)
+        self.assertEqual({"field": "value"}, metadata)
+
+        xmlType = TR.create_file_type("xml", "text/xml")
+        ic(xmlType)
+
+        fileId = TR.create_document_file(documentId, xmlType.id)
+        ic(fileId)
+
+        ok = TR.set_file_metadata(fileId, "creator", "ga-ner-tool")
+        assert (ok)
+
+        metadata = TR.read_file_metadata(fileId)
+        ic(metadata)
+        self.assertEqual("ga-ner-tool", metadata["creator"])
+
+        versions = TR.read_file_versions(fileId)
+        ic(versions)
+
+        file = StringIO('<xml>the contents of this version<</xml>')
+        ic(type(file))
+        versionId = TR.create_version(fileId, file)
+        ic(versionId)
+
+        ok = TR.delete_file(fileId)
+        assert (ok)
+
+        ok = TR.delete_document_metadata(documentId, "field")
+        assert (ok)
+
+        ok = TR.delete_file_type(xmlType.id)
+        assert (ok)
+
+        docId = TR.update_document_externalId(documentId, "new_external_id")
+        ic(docId)
+        self.assertEqual("new_external_id", docId.externalId)
+
+        ok = TR.delete_document(readId)
+        assert (ok)
+
+    def test_document_purge(self):
+        externalId = f'ga:annotationId:{uuid.uuid4()}'
+        documentId = TR.create_document(externalId)
+        ic(documentId)
+        self.assertIsNotNone(documentId)
+
+        ok = TR.purge_document(externalId)
+        assert (ok)
+
+    def purge_file_types(self):
+        if ('localhost' not in TR.base_uri):
+            print("no purging on external textrepo's!!")
+            exit(-1)
+        else:
+            for type in TR.read_file_types():
+                TR.delete_file_type(type.id)
+
+    def purge_all_documents(self):
+        if ('localhost' not in TR.base_uri):
+            print("no purging on external textrepo's!!")
+            exit(-1)
+        else:
+            docPage = TR.read_documents()
+            for document in docPage.items:
+                try:
+                    TR.purge_document(document.externalId)
+                except Exception:
+                    print(f"{TR.base_uri}/rest/documents/{document.id}")
 
 
 if __name__ == '__main__':
