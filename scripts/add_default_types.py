6ccb796ca395dcab3513ecb6b0f34d719f038a90
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2021-07-26 13:38:45 +0200

    add default types to textrepo instance

---
+++ scripts/add_default_types.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from golden_agents.textrepo_client import TextRepoClient
+
+
+def main():
+    TR = TextRepoClient('http://localhost:8080/textrepo/')
+    type1 = TR.create_file_type("xml", "application/xml")
+    type2 = TR.create_file_type("txt", "text/plain")
+    types = TR.read_file_types()
+    print(types)
+
+
+if __name__ == '__main__':
+    main()
