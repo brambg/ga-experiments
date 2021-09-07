5afa529719a7cc9496cea4613adee0fe80d1f508
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2021-09-07 14:30:04 +0200

    show htr/gt text diffs

--- golden_agents/evaluate_analiticcl.py
+++ golden_agents/evaluate_analiticcl.py
@@ -1,11 +1,11 @@
 #!/bin/env python
-
+# -*- coding: utf-8 -*-
 from collections import defaultdict
 from dataclasses import dataclass
 
 import pagexml.parser as px
 from dataclasses_json import dataclass_json
-from icecream import ic
+from shapely.geometry import Polygon
 
 import golden_agents.tools as ga
 
@@ -16,6 +16,13 @@ class EvaluationFile:
     name: str
     htr_path: str
     gt_path: str
+
+
+@dataclass
+class LineInfo:
+    id: str
+    text: str
+    polygon: Polygon
 
 
 def main():
@@ -37,10 +44,45 @@ def main():
     for ef in evaluation_files:
         gt_file = f'../pagexml/{ef.gt_path}'
         gt_scan = px.parse_pagexml_file(gt_file)
-        ic(gt_scan.id)
+        gt_line_info = []
+        for l in gt_scan.get_lines():
+            if l.text:
+                gt_line_info.append(LineInfo(
+                    id=l.id,
+                    text=l.text,
+                    polygon=(Polygon(l.coords.points))
+                ))
         htr_file = f'../pagexml/{ef.htr_path}'
         htr_scan = px.parse_pagexml_file(htr_file)
-        ic(htr_scan.id)
+        htr_line_info = []
+        for l in htr_scan.get_lines():
+            if l.text:
+                htr_line_info.append(LineInfo(
+                    id=l.id,
+                    text=l.text,
+                    polygon=(Polygon(l.coords.points))
+                ))
+        for li in gt_line_info:
+            if not li.polygon.is_valid:
+                print(li)
+                break
+            htr_match = htr_line_info[0]
+            largest_overlap = 0
+            for htr_li in htr_line_info:
+                if not htr_li.polygon.is_valid:
+                    print(htr_li)
+                    break
+                overlap = li.polygon.intersection(htr_li.polygon).area
+                if overlap > largest_overlap:
+                    largest_overlap = overlap
+                    htr_match = htr_li
+            if li.text != htr_match.text:
+                # print(f"GT line {li.id}<{li.text}> matches with HTR line {htr_match.id}<{htr_match.text}>")
+                htr_id = f"{ef.htr_path}#{htr_match.id}"
+                gt_id = f"{ef.gt_path}#{li.id}"
+                print(f"HTR: {htr_id.ljust(42)} | {htr_match.text}")
+                print(f"GT : {gt_id.ljust(42)} | {li.text}")
+                print()
 
 
 if __name__ == '__main__':
